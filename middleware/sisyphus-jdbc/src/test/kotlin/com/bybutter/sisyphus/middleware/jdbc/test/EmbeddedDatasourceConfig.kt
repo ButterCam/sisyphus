031c2d239e3058809101addcc9822df8408e9e18
Kanro <higan@live.cn>
2020-05-21 12:03:15 +0800

    Fix jdbc tests

--- middleware/sisyphus-jdbc/src/test/kotlin/com/bybutter/sisyphus/middleware/jdbc/test/EmbeddedDatasourceConfig.kt
+++ middleware/sisyphus-jdbc/src/test/kotlin/com/bybutter/sisyphus/middleware/jdbc/test/EmbeddedDatasourceConfig.kt
@@ -1,20 +1,11 @@
 package com.bybutter.sisyphus.middleware.jdbc.test
 
-import com.bybutter.sisyphus.middleware.jdbc.DefaultDslContextFactory
-import com.bybutter.sisyphus.middleware.jdbc.DslContextFactory
 import com.bybutter.sisyphus.middleware.jdbc.JdbcDatabaseProperty
-import com.bybutter.sisyphus.middleware.jdbc.JooqConfigInterceptor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class EmbeddedDatasourceConfig {
-    @Bean
-    fun dslContextFactory(@Autowired configInterceptors: List<JooqConfigInterceptor>): DslContextFactory {
-        return DefaultDslContextFactory(configInterceptors)
-    }
-
     @Bean
     fun embeddedDatasource(): JdbcDatabaseProperty {
         return JdbcDatabaseProperty(url = "jdbc:h2:mem:test")
