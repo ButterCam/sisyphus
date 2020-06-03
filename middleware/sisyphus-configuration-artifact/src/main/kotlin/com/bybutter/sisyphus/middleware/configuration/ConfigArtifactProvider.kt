97896a1bf3c86d23ebbb9d94fb1c22e9f365e420
Kanro <higan@live.cn>
2020-06-03 17:04:16 +0800

    Add defer logger for 'ConfigArtifactProvider'

--- middleware/sisyphus-configuration-artifact/src/main/kotlin/com/bybutter/sisyphus/middleware/configuration/ConfigArtifactProvider.kt
+++ middleware/sisyphus-configuration-artifact/src/main/kotlin/com/bybutter/sisyphus/middleware/configuration/ConfigArtifactProvider.kt
@@ -4,25 +4,27 @@ import com.bybutter.sisyphus.middleware.configuration.maven.Aether
 import com.bybutter.sisyphus.spi.ServiceLoader
 import java.net.URLClassLoader
 import org.eclipse.aether.resolution.ArtifactResult
-import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.context.event.ApplicationPreparedEvent
 import org.springframework.boot.context.properties.bind.Binder
 import org.springframework.boot.context.properties.source.ConfigurationPropertySources
 import org.springframework.boot.env.EnvironmentPostProcessor
+import org.springframework.boot.logging.DeferredLog
+import org.springframework.context.ApplicationListener
 import org.springframework.core.env.ConfigurableEnvironment
 
 /**
  * Provide config artifacts for [ConfigArtifactPropertyExporter], it will download artifacts from configured maven repository
  * and load it dynamically.
  */
-class ConfigArtifactProvider : EnvironmentPostProcessor {
+class ConfigArtifactProvider : EnvironmentPostProcessor, ApplicationListener<ApplicationPreparedEvent> {
     override fun postProcessEnvironment(environment: ConfigurableEnvironment, application: SpringApplication) {
         val binder = Binder(ConfigurationPropertySources.from(environment.propertySources))
         val properties = binder.bind("sisyphus", SisyphusProperty::class.java)
-                .orElse(null) ?: return
+            .orElse(null) ?: return
 
         if (properties.config.artifacts.isEmpty()) {
-            logger.warn("Skip load config artifacts due to empty artifacts list, artifacts list can be set by 'next.config.artifacts' property.")
+            logger.warn("Skip load config artifacts due to empty artifacts list, artifacts list can be set by 'sisyphus.config.artifacts' property.")
             return
         }
 
@@ -53,7 +55,7 @@ class ConfigArtifactProvider : EnvironmentPostProcessor {
 
         val artifacts = properties.config.artifacts.flatMap {
             try {
-                logger.info("Resolving config artifact '$it'.")
+                logger.debug("Resolving config artifact '$it'.")
                 aether.resolveDependencies(it).artifactResults
             } catch (e: Exception) {
                 logger.error("Can't resolve config artifact '$it', cause by exception", e)
@@ -62,9 +64,10 @@ class ConfigArtifactProvider : EnvironmentPostProcessor {
         }
 
         val jarFiles = artifacts.map {
-            logger.info("Config artifact '${it.artifact}' loaded.")
             it.artifact.file.toURI().toURL()
         }.distinct()
+
+        logger.info("${jarFiles.size} config artifacts loaded: ${artifacts.joinToString(", ") { "'${it.artifact}'" }}.")
 
         val classLoader = URLClassLoader(jarFiles.toTypedArray(), ConfigArtifactProvider::class.java.classLoader)
         val propertySources = ServiceLoader.load(ConfigArtifactPropertyExporter::class.java)
@@ -78,9 +81,13 @@ class ConfigArtifactProvider : EnvironmentPostProcessor {
         }
     }
 
+    override fun onApplicationEvent(event: ApplicationPreparedEvent) {
+        logger.switchTo(ConfigArtifactProvider::class.java)
+    }
+
     companion object {
         private val aether = Aether()
 
-        private val logger = LoggerFactory.getLogger(ConfigArtifactProvider::class.java)
+        private val logger = DeferredLog()
     }
 }
