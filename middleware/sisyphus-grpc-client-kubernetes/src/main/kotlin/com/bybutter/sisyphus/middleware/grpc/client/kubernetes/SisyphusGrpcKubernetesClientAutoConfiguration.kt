eebb2c40651504d376de22757c6cb11a8c682e95
wz <wangzheng@bybutter.com>
2020-07-07 15:22:54 +0800

    Remove invalid code

--- middleware/sisyphus-grpc-client-kubernetes/src/main/kotlin/com/bybutter/sisyphus/middleware/grpc/client/kubernetes/SisyphusGrpcKubernetesClientAutoConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.bybutter.sisyphus.middleware.grpc.client.kubernetes
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan(basePackageClasses = [SisyphusGrpcKubernetesClientAutoConfiguration::class])
-class SisyphusGrpcKubernetesClientAutoConfiguration
