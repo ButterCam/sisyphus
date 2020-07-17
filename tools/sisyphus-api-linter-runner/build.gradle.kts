6f7474fe98314d47e8717084371fd21eed58b99e
Kanro <higan@live.cn>
2020-07-17 15:27:08 +0800

    Make api linter runner as simple java library

--- tools/sisyphus-api-linter-runner/build.gradle.kts
+++ tools/sisyphus-api-linter-runner/build.gradle.kts
@@ -2,8 +2,6 @@ tools
 
 plugins {
     `java-library`
-    `java-gradle-plugin`
-    id("com.gradle.plugin-publish")
 }
 
 description = "Runner and executable manager for Google API Linter on java"
