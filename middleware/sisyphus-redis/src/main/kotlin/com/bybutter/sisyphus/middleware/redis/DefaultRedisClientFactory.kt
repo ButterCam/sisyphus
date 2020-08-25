d9cd59f5e9286e1b445cf7db8f39b8b53c844837
wz <wangzheng@bybutter.com>
2020-08-25 12:00:15 +0800

    Optimize code and update gradle

--- middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/DefaultRedisClientFactory.kt
+++ middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/DefaultRedisClientFactory.kt
@@ -12,8 +12,7 @@ open class DefaultRedisClientFactory : RedisClientFactory {
 
     protected open fun createRedisClient(host: String, port: Int, property: RedisProperty): RedisClient {
         return clients.getOrPut("$host:$port") {
-            val database = property.database ?: 0
-            RedisClient.create(RedisURI.Builder.redis(host, port).withPassword(property.password).withDatabase(database).build())
+            RedisClient.create(RedisURI.Builder.redis(host, port).withPassword(property.password).withDatabase(property.database ?: 0).build())
         }
     }
 }
