1935b36857aa2a0b9b36003514bc1f7858220a94
sustly <279719732@qq.com>
2020-09-15 17:20:12 +0800

    :rotating_light: Make ktlint happy

--- starter/sisyphus-grpc-server-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/SisyphusGrpcServerAutoConfiguration.kt
+++ starter/sisyphus-grpc-server-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/SisyphusGrpcServerAutoConfiguration.kt
@@ -1,7 +1,6 @@
 package com.bybutter.sisyphus.starter.grpc
 
 import com.bybutter.sisyphus.middleware.grpc.autoconfigure.SisyphusGrpcClientAutoConfiguration
-import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.context.annotation.ComponentScan
 
