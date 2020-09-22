618f88f9f8eb817c1e2abe711a342542b0c6a71c
future <zhaoy_xin@163.com>
2020-09-22 18:21:42 +0800

    :art: Improve code

--- starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/utils/SwaggerDescription.kt
+++ starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/utils/SwaggerDescription.kt
@@ -2,13 +2,9 @@ package com.bybutter.sisyphus.starter.grpc.transcoding.support.swagger.utils
 
 import com.bybutter.sisyphus.collection.contentEquals
 import com.bybutter.sisyphus.protobuf.primitives.FileDescriptorProto
-import java.util.regex.Pattern
 
 object SwaggerDescription {
-
-    private val pattern: Pattern by lazy {
-        Pattern.compile("""(\(-- api-linter:.[\s\S]+? --\))""")
-    }
+    private val regex = """(\(-- api-linter:.[\s\S]+? --\))""".toRegex()
     /**
      *  Get the corresponding proto file comment according to path.
      * */
@@ -17,6 +13,9 @@ object SwaggerDescription {
             location.path.contentEquals(path)
         }
         location ?: return null
-        return pattern.matcher(listOf(location.leadingComments, location.trailingComments).filter { it.isNotBlank() }.joinToString("\n\n")).replaceAll("").trim()
+        return listOf(location.leadingComments, location.trailingComments).filter { it.isNotBlank() }
+                .joinToString("\n\n")
+                .replace(regex, "")
+                .trim()
     }
 }
