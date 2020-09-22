30c0c9fdb6f8506c365a28ecc2750f30672b1f06
future <zhaoy_xin@163.com>
2020-09-22 15:25:51 +0800

    :art: Filter out api-linter comments

--- starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/utils/SwaggerDescription.kt
+++ starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/utils/SwaggerDescription.kt
@@ -2,8 +2,13 @@ package com.bybutter.sisyphus.starter.grpc.transcoding.support.swagger.utils
 
 import com.bybutter.sisyphus.collection.contentEquals
 import com.bybutter.sisyphus.protobuf.primitives.FileDescriptorProto
+import java.util.regex.Pattern
 
 object SwaggerDescription {
+
+    private val pattern: Pattern by lazy {
+        Pattern.compile("""(\(-- api-linter:.[\s\S]+? --\))""")
+    }
     /**
      *  Get the corresponding proto file comment according to path.
      * */
@@ -12,6 +17,6 @@ object SwaggerDescription {
             location.path.contentEquals(path)
         }
         location ?: return null
-        return listOf(location.leadingComments, location.trailingComments).filter { it.isNotBlank() }.joinToString("\n\n")
+        return pattern.matcher(listOf(location.leadingComments, location.trailingComments).filter { it.isNotBlank() }.joinToString("\n\n")).replaceAll("").trim()
     }
 }
