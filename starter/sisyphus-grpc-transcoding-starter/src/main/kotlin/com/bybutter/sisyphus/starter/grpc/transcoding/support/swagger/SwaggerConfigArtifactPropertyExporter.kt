2b504cb0f23d999ab1a35539f6654bbae892616f
future <54358281+yuxin-zhao@users.noreply.github.com>
2020-09-22 18:54:49 +0800

    :art: Filter out api-linter comments (#83)
    * :art: Filter out api-linter comments
    
    * :art: Improve code

--- starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/SwaggerConfigArtifactPropertyExporter.kt
+++ starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/swagger/SwaggerConfigArtifactPropertyExporter.kt
@@ -2,6 +2,10 @@ package com.bybutter.sisyphus.starter.grpc.transcoding.support.swagger
 
 import com.bybutter.sisyphus.middleware.configuration.ConfigFormatFilePropertyExporter
 
+/**
+ *  The configuration of swagger uses 'swagger/config' by default.
+ *  This configuration can be overridden in the application.
+ * */
 object SwaggerConfigArtifactPropertyExporter : ConfigFormatFilePropertyExporter() {
     override val names: Collection<String> = listOf("swagger/config")
 }
