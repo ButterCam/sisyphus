b13a08fc4d1ae0a5f46e68032d36247d83db4049
GuoDuanLZ <wangzheng@bybutter.com>
2020-09-25 11:30:04 +0800

    :art: Modify middleware name to qualifier annotation (#82)
    * middleware
    
    * Middleware
    
    * :art: Modify name to qualify
    
    * :art: Modify name to qualify
    
    * :art: Optimize redis connection
    
    * :art: Fixed jdbc test bug
    
    * :art: Reformat code

--- middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/RedisConnectionRegistrar.kt
+++ middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/RedisConnectionRegistrar.kt
@@ -1,14 +1,20 @@
 package com.bybutter.sisyphus.middleware.redis
 
+import io.lettuce.core.RedisClient
 import io.lettuce.core.api.StatefulRedisConnection
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.beans.factory.getBeansOfType
+import org.springframework.beans.factory.support.AutowireCandidateQualifier
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
 import org.springframework.beans.factory.support.BeanDefinitionRegistry
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.boot.context.properties.bind.Binder
 import org.springframework.context.EnvironmentAware
 import org.springframework.core.env.Environment
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -35,16 +41,33 @@ class RedisConnectionRegistrar : BeanDefinitionRegistryPostProcessor, Environmen
         if (properties.isEmpty()) return
 
         for ((name, property) in properties) {
-            val beanName = property.name ?: "$BEAN_NAME_PREFIX:$name"
+            val beanName = "$BEAN_NAME_PREFIX:$name"
+            val connectionFactoryBeanName = "$CONNECTION_FACTORY_BEAN_NAME_PREFIX:$name"
+
+            val redisConnectionFactory = LettuceConnectionFactory(convertToConfig(property))
+            val redisConnectionFactoryBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(RedisConnectionFactory::class.java) {
+                redisConnectionFactory
+            }.setDestroyMethodName("destroy").beanDefinition
+            redisConnectionFactoryBeanDefinition.addQualifier(AutowireCandidateQualifier(property.qualifier))
+            registry.registerBeanDefinition(connectionFactoryBeanName, redisConnectionFactoryBeanDefinition)
+
             val beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(StatefulRedisConnection::class.java) {
-                val factory = beanFactory.getBean(RedisClientFactory::class.java)
-                factory.createClient(property).connect()
+                RedisClient.create(redisConnectionFactory.clientResources).connect()
             }.setDestroyMethodName("close").beanDefinition
+            beanDefinition.addQualifier(AutowireCandidateQualifier(property.qualifier))
             registry.registerBeanDefinition(beanName, beanDefinition)
+        }
+    }
+
+    private fun convertToConfig(property: RedisProperty): RedisStandaloneConfiguration {
+        return RedisStandaloneConfiguration(property.host, property.port).apply {
+            this.password = RedisPassword.of(property.password)
+            this.database = property.database ?: 0
         }
     }
 
     companion object {
         private const val BEAN_NAME_PREFIX = "sisyphus:redis"
+        private const val CONNECTION_FACTORY_BEAN_NAME_PREFIX = "sisyphus:redis:connection:factory"
     }
 }
