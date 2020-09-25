b1ab14d3086d54a38dc3d796ca28910b1c80588e
sustly <279719732@qq.com>
2020-09-25 10:19:48 +0800

    :bug: ConditionalOnMissingBean value is StatefulRedisConnection

--- middleware/sisyphus-distributed-lock/src/main/kotlin/com/bybutter/middleware/distributed/lock/autoconfigure/RedisLockAutoConfiguration.kt
+++ middleware/sisyphus-distributed-lock/src/main/kotlin/com/bybutter/middleware/distributed/lock/autoconfigure/RedisLockAutoConfiguration.kt
@@ -1,13 +1,13 @@
 package com.bybutter.middleware.distributed.lock.autoconfigure
 
+import io.lettuce.core.api.StatefulRedisConnection
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.core.StringRedisTemplate
 
 @ComponentScan(basePackageClasses = [RedisLockAutoConfiguration::class])
 @Configuration
-@ConditionalOnMissingBean(value = [StringRedisTemplate::class])
+@ConditionalOnMissingBean(value = [StatefulRedisConnection::class])
 @AutoConfigureAfter(name = ["com.bybutter.sisyphus.middleware.redis.autoconfigure.SisyphusRedisAutoConfiguration"])
 class RedisLockAutoConfiguration
