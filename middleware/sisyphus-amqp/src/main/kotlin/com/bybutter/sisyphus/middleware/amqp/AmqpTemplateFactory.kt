9650122c43d8581f68911db3c653183e00c7b29e
Kanro <higan@live.cn>
2020-11-30 14:18:39 +0800

    :sparkles: Support customized request logger

--- middleware/sisyphus-amqp/src/main/kotlin/com/bybutter/sisyphus/middleware/amqp/AmqpTemplateFactory.kt
+++ middleware/sisyphus-amqp/src/main/kotlin/com/bybutter/sisyphus/middleware/amqp/AmqpTemplateFactory.kt
@@ -1,7 +1,6 @@
 package com.bybutter.sisyphus.middleware.amqp
 
 import org.springframework.amqp.core.AmqpTemplate
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
 
 interface AmqpTemplateFactory {
     fun createTemplate(property: MessageQueueProperty): AmqpTemplate
--- middleware/sisyphus-amqp/src/main/kotlin/com/bybutter/sisyphus/middleware/amqp/DefaultAmqpTemplateFactory.kt
+++ middleware/sisyphus-amqp/src/main/kotlin/com/bybutter/sisyphus/middleware/amqp/DefaultAmqpTemplateFactory.kt
@@ -3,7 +3,6 @@ package com.bybutter.sisyphus.middleware.amqp
 import com.bybutter.sisyphus.jackson.Json
 import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory.ConfirmType
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
