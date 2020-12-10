8b6c61035b97f1dfba814ab3f137f735df8515d1
Kanro <higan@live.cn>
2020-12-10 15:14:44 +0800

    :heavy_minus_sign: Remove slf4j12 from hbase dependencies

--- middleware/sisyphus-hbase/build.gradle.kts
+++ middleware/sisyphus-hbase/build.gradle.kts
@@ -9,6 +9,8 @@ description = "Middleware for using HBase in Sisyphus Project"
 dependencies {
     api(project(":lib:sisyphus-dto"))
     api(project(":lib:sisyphus-jackson"))
-    api(Dependencies.hbase)
+    api(Dependencies.hbase) {
+        exclude("org.slf4j", "slf4j-log4j12")
+    }
     implementation(project(":lib:sisyphus-common"))
 }
