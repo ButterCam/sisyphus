0c66383aa041b3ec2906f61aba5a99e974caaa46
Kanro <higan@live.cn>
2020-12-11 14:00:53 +0800

    :sparkles: Support channel builder interceptor

--- middleware/sisyphus-grpc-client-kubernetes/src/main/kotlin/com/bybutter/sisyphus/middleware/grpc/client/kubernetes/KubernetesClientRepository.kt
+++ middleware/sisyphus-grpc-client-kubernetes/src/main/kotlin/com/bybutter/sisyphus/middleware/grpc/client/kubernetes/KubernetesClientRepository.kt
@@ -1,7 +1,10 @@
 package com.bybutter.sisyphus.middleware.grpc.client.kubernetes
 
+import com.bybutter.sisyphus.middleware.grpc.ChannelBuilderInterceptor
 import com.bybutter.sisyphus.middleware.grpc.ClientBuilderInterceptor
+import com.bybutter.sisyphus.middleware.grpc.ClientRegistrar
 import com.bybutter.sisyphus.middleware.grpc.ClientRepository
+import com.bybutter.sisyphus.middleware.grpc.ManagedChannelLifecycle
 import com.bybutter.sisyphus.protobuf.ProtoTypes
 import com.bybutter.sisyphus.rpc.CallOptionsInterceptor
 import com.bybutter.sisyphus.spring.BeanUtils
@@ -15,6 +18,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.beans.factory.getBean
 import org.springframework.beans.factory.support.AbstractBeanDefinition
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
 import org.springframework.core.env.Environment
@@ -39,6 +43,13 @@ class KubernetesClientRepository : ClientRepository {
         logger.debug("Detect application is running in kubernetes namespace $namespace.")
         val beanDefinitionList = arrayListOf<AbstractBeanDefinition>()
         val registerServiceNames = ProtoTypes.getRegisteredServiceNames()
+
+        val channelBuilderInterceptors = BeanUtils.getSortedBeans(beanFactory, ChannelBuilderInterceptor::class.java)
+        val builderInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientBuilderInterceptor::class.java)
+        val clientInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientInterceptor::class.java)
+        val optionsInterceptors = BeanUtils.getSortedBeans(beanFactory, CallOptionsInterceptor::class.java)
+        val managedChannelLifecycle = beanFactory.getBean<ManagedChannelLifecycle>(ClientRegistrar.QUALIFIER_AUTO_CONFIGURED_GRPC_CHANNEL_LIFECYCLE)
+
         for (serviceName in registerServiceNames) {
             val list = try {
                 api.listNamespacedService(namespace, null, null, null, null, "sisyphus/$serviceName", null, null, null, null)
@@ -54,14 +65,11 @@ class KubernetesClientRepository : ClientRepository {
             }?.port ?: continue
             val host = k8sService.metadata?.name ?: continue
             logger.info("GRPC service '$serviceName' discovered in kubernetes service '$host:$port'.")
-            val channel = createGrpcChannel("$host:$port", beanFactory)
+            val channel = createGrpcChannel("$host:$port", channelBuilderInterceptors.values, managedChannelLifecycle)
             val service = ProtoTypes.getRegisterService(serviceName) ?: continue
             val client = getClientFromService(service)
             val stub = getStubFromService(service)
             val clientBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(client as Class<Any>) {
-                val builderInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientBuilderInterceptor::class.java)
-                val clientInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientInterceptor::class.java)
-                val optionsInterceptors = BeanUtils.getSortedBeans(beanFactory, CallOptionsInterceptor::class.java)
                 interceptStub(createGrpcClient(stub, channel, optionsInterceptors.values, CallOptions.DEFAULT), builderInterceptors.values, clientInterceptors.values)
             }
             beanDefinitionList.add(clientBeanDefinition.beanDefinition)
--- middleware/sisyphus-grpc-client/src/main/kotlin/com/bybutter/sisyphus/middleware/grpc/LocalClientRepository.kt
+++ middleware/sisyphus-grpc-client/src/main/kotlin/com/bybutter/sisyphus/middleware/grpc/LocalClientRepository.kt
@@ -7,6 +7,7 @@ import com.bybutter.sisyphus.spring.BeanUtils
 import io.grpc.CallOptions
 import io.grpc.ClientInterceptor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.beans.factory.getBean
 import org.springframework.beans.factory.support.AbstractBeanDefinition
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
 import org.springframework.core.annotation.AnnotationUtils
@@ -17,7 +18,14 @@ class LocalClientRepository : ClientRepository {
 
     override fun listClientBeanDefinition(beanFactory: ConfigurableListableBeanFactory, environment: Environment): List<AbstractBeanDefinition> {
         val localPort = environment.getProperty(GrpcServerConstants.GRPC_PORT_PROPERTY, Int::class.java, GrpcServerConstants.DEFAULT_GRPC_PORT)
-        val localChannel = createGrpcChannel("localhost:$localPort", beanFactory)
+
+        val channelBuilderInterceptors = BeanUtils.getSortedBeans(beanFactory, ChannelBuilderInterceptor::class.java)
+        val builderInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientBuilderInterceptor::class.java)
+        val clientInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientInterceptor::class.java)
+        val optionsInterceptors = BeanUtils.getSortedBeans(beanFactory, CallOptionsInterceptor::class.java)
+        val managedChannelLifecycle = beanFactory.getBean<ManagedChannelLifecycle>(ClientRegistrar.QUALIFIER_AUTO_CONFIGURED_GRPC_CHANNEL_LIFECYCLE)
+
+        val localChannel = createGrpcChannel("localhost:$localPort", channelBuilderInterceptors.values, managedChannelLifecycle)
         val beanDefinitionList = arrayListOf<AbstractBeanDefinition>()
         for (serviceName in beanFactory.getBeanNamesForAnnotation(RpcServiceImpl::class.java)) {
             val serviceBeanDefinition = beanFactory.getBeanDefinition(serviceName)
@@ -26,9 +34,6 @@ class LocalClientRepository : ClientRepository {
             val service = rpcService.client.java.declaringClass
             val stub = getStubFromService(service)
             val clientBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(rpcService.client.java as Class<Any>) {
-                val builderInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientBuilderInterceptor::class.java)
-                val clientInterceptors = BeanUtils.getSortedBeans(beanFactory, ClientInterceptor::class.java)
-                val optionsInterceptors = BeanUtils.getSortedBeans(beanFactory, CallOptionsInterceptor::class.java)
                 interceptStub(createGrpcClient(stub, localChannel, optionsInterceptors.values, CallOptions.DEFAULT), builderInterceptors.values, clientInterceptors.values)
             }
             beanDefinitionList.add(clientBeanDefinition.beanDefinition)
