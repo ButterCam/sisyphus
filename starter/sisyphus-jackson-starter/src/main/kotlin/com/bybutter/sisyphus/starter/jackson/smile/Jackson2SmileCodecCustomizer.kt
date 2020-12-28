816eda87c3025c7a3f0a0bf827fcf64de7b08c35
Kanro <higan@live.cn>
2020-12-28 18:02:20 +0800

    :bug: Fix jackson smile encoder/decoder (#113)

--- starter/sisyphus-jackson-starter/src/main/kotlin/com/bybutter/sisyphus/starter/jackson/smile/Jackson2SmileCodecCustomizer.kt
+++ starter/sisyphus-jackson-starter/src/main/kotlin/com/bybutter/sisyphus/starter/jackson/smile/Jackson2SmileCodecCustomizer.kt
@@ -1,6 +1,6 @@
 package com.bybutter.sisyphus.starter.jackson.smile
 
-import com.bybutter.sisyphus.jackson.Json
+import com.bybutter.sisyphus.jackson.Smile
 import org.springframework.boot.web.codec.CodecCustomizer
 import org.springframework.http.codec.CodecConfigurer
 import org.springframework.http.codec.json.Jackson2SmileDecoder
@@ -9,8 +9,8 @@ import org.springframework.util.MimeType
 
 class Jackson2SmileCodecCustomizer : CodecCustomizer {
     override fun customize(configurer: CodecConfigurer) {
-        configurer.defaultCodecs().jackson2SmileEncoder(Jackson2SmileEncoder(Json.mapper, *DEFAULT_SMILE_MIME_TYPES))
-        configurer.defaultCodecs().jackson2SmileDecoder(Jackson2SmileDecoder(Json.mapper, *DEFAULT_SMILE_MIME_TYPES))
+        configurer.defaultCodecs().jackson2SmileEncoder(Jackson2SmileEncoder(Smile.mapper, *DEFAULT_SMILE_MIME_TYPES))
+        configurer.defaultCodecs().jackson2SmileDecoder(Jackson2SmileDecoder(Smile.mapper, *DEFAULT_SMILE_MIME_TYPES))
     }
 
     companion object {
