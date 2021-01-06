4591898f1da4252b505664f32b2129e4aa8d9401
wz <wangzheng@bybutter.com>
2021-01-06 17:26:53 +0800

    :art: Format code

--- middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/utils/Page.kt
+++ middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/utils/Page.kt
@@ -9,16 +9,7 @@ import org.jooq.SelectLimitStep
 data class Page<T>(var data: List<T>, var nextToken: String?, var total: Int? = null)
 
 fun <T : Record> SelectLimitStep<T>.withPaging(pageToken: String, pageSize: Int, needTotal: Boolean = true): Page<T> {
-    val offset = OffsetPaging(pageToken)?.offset
-    val size = if (pageSize in 1..30) {
-        pageSize
-    } else {
-        30
-    }
-    val count = if (needTotal) this.count() else null
-    val data = this.offset(offset).limit(size).fetchInto(this.recordType)
-    val next = OffsetPaging(pageToken).nextPage(data.size, size)
-    return Page(data, next, count)
+    return withTypePaging(this.recordType, pageToken, pageSize, needTotal) as Page<T>
 }
 
 inline fun <T : Record, reified R> SelectLimitStep<T>.withTypePaging(pageToken: String, pageSize: Int, needTotal: Boolean = true): Page<R> {
