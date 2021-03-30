1e12bd4dfc8eba0e87603ec1146f180ae0e7a78f
Kanro <higan@live.cn>
2021-03-30 15:57:42 +0800

    :apple: Add Apple silicon support for protobuf compiler (#212)
    * :apple: Add Apple silicon support for protobuf compiler
    
    * :apple: Add Apple silicon support for protobuf compiler

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `java-library`
     `kotlin-dsl`
     id("idea")
-    id("com.bybutter.sisyphus.project") version "1.1.4"
+    id("com.bybutter.sisyphus.project") version "1.2.0"
 }
 
 dependencies {
--- tools/sisyphus-protoc/src/main/kotlin/com/bybutter/sisyphus/protobuf/compiler/ProtocRunner.kt
+++ tools/sisyphus-protoc/src/main/kotlin/com/bybutter/sisyphus/protobuf/compiler/ProtocRunner.kt
@@ -1,30 +1,35 @@
 package com.bybutter.sisyphus.protobuf.compiler
 
 import com.github.os72.protocjar.Protoc
+import com.github.os72.protocjar.ProtocVersion
 import com.google.protobuf.DescriptorProtos
 import java.io.File
+import java.io.FileNotFoundException
 import java.nio.file.Files
 
-open class ProtocRunner(val version: String = "3.11.4") {
-    companion object : ProtocRunner()
-
+object ProtocRunner {
     fun generate(protoPath: File, source: Collection<String>): DescriptorProtos.FileDescriptorSet {
         if (source.isEmpty()) {
             return DescriptorProtos.FileDescriptorSet.newBuilder().build()
         }
 
         val outputFile = Files.createTempFile("out", ".pb")
-        val result = Protoc.runProtoc(
-            arrayOf(
-                "-v$version",
-                "-o$outputFile",
-                "-I$protoPath",
-                "--include_imports",
-                "--include_source_info",
-                *source.toTypedArray()
-            ),
-            System.out, System.out
+        val arguments = arrayOf(
+            "-v${ProtocVersion.PROTOC_VERSION.mVersion}",
+            "-o$outputFile",
+            "-I$protoPath",
+            "--include_imports",
+            "--include_source_info",
+            *source.toTypedArray()
         )
+
+        val result = try {
+            Protoc.extractProtoc(ProtocVersion.PROTOC_VERSION, false)
+            Protoc.runProtoc(arguments, System.out, System.out)
+        } catch (e: FileNotFoundException) {
+            Protoc.runProtoc("protoc", arguments.toList(), System.out, System.out)
+        }
+
         if (result != 0) {
             throw IllegalStateException("Protoc return '$result' with not zero value.")
         }
