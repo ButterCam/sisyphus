f8fefeaf17afb783450606b5f7a750f6b9d1636f
wz <wangzheng@bybutter.com>
2021-05-17 18:57:48 +0800

    :sparkles: Add distributedLock try lock

--- middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/util/DistributedLock.kt
+++ middleware/sisyphus-redis/src/main/kotlin/com/bybutter/sisyphus/middleware/redis/util/DistributedLock.kt
@@ -5,31 +5,62 @@ import io.lettuce.core.SetArgs
 import io.lettuce.core.api.StatefulRedisConnection
 import io.netty.util.HashedWheelTimer
 import io.netty.util.Timeout
-import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
-
 
 class DistributedLock(private val redis: StatefulRedisConnection<String, String>) {
     private val timer = HashedWheelTimer()
-    private val renewExpirationMap = mutableMapOf<String, LockInfo>()
+    private val renewExpirationMap = ConcurrentHashMap<String, LockInfo>()
 
     suspend fun <T> lock(key: String, leaseTime: Long, timeUnit: TimeUnit, action: suspend () -> T?): T? {
-        return try {
-            if (lock(key, leaseTime, timeUnit)) {
+        return if (lock(key, leaseTime, timeUnit)) {
+            try {
                 action()
-            } else {
-                throw Exception("加锁失败~")
+            } finally {
+                unlock(key)
             }
-        } finally {
-            unlock(key)
+        } else {
+            throw Exception("加锁失败~")
         }
+    }
+
+    suspend fun <T> tryLock(
+        key: String,
+        waitTime: Long,
+        leaseTime: Long,
+        timeUnit: TimeUnit,
+        action: suspend () -> T?
+    ): T? {
+        return if (tryLock(key, waitTime, leaseTime, timeUnit)) {
+            try {
+                action()
+            } finally {
+                unlock(key)
+            }
+        } else {
+            throw Exception("加锁失败~")
+        }
+    }
+
+    suspend fun tryLock(key: String, waitTime: Long, leaseTime: Long, timeUnit: TimeUnit): Boolean {
+        val endTime = System.currentTimeMillis() + timeUnit.toMillis(waitTime)
+        while (System.currentTimeMillis() < endTime) {
+            if (lock(key, leaseTime, timeUnit))
+                return true
+            delay(100)
+        }
+        return false
     }
 
     suspend fun lock(key: String, leaseTime: Long, timeUnit: TimeUnit): Boolean {
         val value = UUID.randomUUID().toString()
-        return (redis.reactive().set(key, value, SetArgs().nx().ex(timeUnit.toSeconds(leaseTime)))
-            .awaitFirst() == null).also {
+        return (
+            redis.reactive().set(key, value, SetArgs().nx().ex(timeUnit.toSeconds(leaseTime)))
+                .awaitFirstOrNull() != null
+            ).also {
             if (it) {
                 scheduleRenewExpiration(key, value, leaseTime, timeUnit)
             }
@@ -44,27 +75,30 @@ class DistributedLock(private val redis: StatefulRedisConnection<String, String>
 
     private fun renewExpiration(key: String, leaseTime: Long, timeUnit: TimeUnit) {
         val lockInfo = renewExpirationMap[key] ?: return
-        lockInfo.timeout = timer.newTimeout({
-            redis.sync().eval<String>(
-                "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('expire', KEYS[1],ARGV[2]) else return 0 end",
-                ScriptOutputType.INTEGER,
-                arrayOf(key),
-                lockInfo.value,
-                leaseTime.toString()
-            ).takeIf { it == "1" }?.let {
-                renewExpiration(key, leaseTime, timeUnit)
-            }
-        }, leaseTime * 2 / 3, timeUnit)
+        lockInfo.timeout = timer.newTimeout(
+            {
+                redis.sync().eval<Long>(
+                    "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('expire', KEYS[1],ARGV[2]) else return 0 end",
+                    ScriptOutputType.INTEGER,
+                    arrayOf(key),
+                    lockInfo.value,
+                    leaseTime.toString()
+                ).takeIf { it == 1L }?.let {
+                    renewExpiration(key, leaseTime, timeUnit)
+                }
+            },
+            leaseTime * 2 / 3, timeUnit
+        )
     }
 
     suspend fun unlock(key: String) {
         val lockInfo = renewExpirationMap[key] ?: return
-        redis.reactive().eval<String>(
+        redis.reactive().eval<Long>(
             "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end",
             ScriptOutputType.INTEGER,
             arrayOf(key),
             lockInfo.value
-        ).awaitFirst().takeIf { it == "1" }?.let {
+        ).awaitFirstOrNull().takeIf { it == 1L }?.let {
             cancelRenewExpiration(key)
         }
     }
