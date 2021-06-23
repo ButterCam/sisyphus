387d256d4b4df5d1fb6492318feaa00b3919499f
devkanro <higan@live.cn>
2021-06-23 10:59:01 +0800

    :bug: Fix sisyphus project plugin three-part extension

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     `java-library`
     `kotlin-dsl`
     id("idea")
-    id("com.bybutter.sisyphus.project") version "1.2.10"
+    id("com.bybutter.sisyphus.project") version "1.2.12"
 }
 
 dependencies {
-    implementation(platform("com.bybutter.sisyphus:sisyphus-dependencies:higan-SNAPSHOT"))
+    implementation(platform("com.bybutter.sisyphus:sisyphus-dependencies:master-SNAPSHOT"))
     implementation("com.bybutter.sisyphus.tools:sisyphus-protobuf-gradle-plugin")
     implementation("com.bybutter.sisyphus.tools:sisyphus-project-gradle-plugin")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
