7718475fb13f31c8046e86128270b2a680a32a09
devkanro <higan@live.cn>
2021-11-29 19:16:09 +0800

    :fire: Remove TableBasedOrderByBuilder

--- middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/order/CustomOrderByBuilder.kt
+++ middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/order/CustomOrderByBuilder.kt
@@ -2,17 +2,27 @@ package com.bybutter.sisyphus.middleware.jdbc.support.proto.order
 
 import com.bybutter.sisyphus.string.toCamelCase
 import org.jooq.Field
+import org.jooq.SortField
 
 class CustomOrderByBuilder : OrderByBuilder() {
+    private val defaultOrder = mutableListOf<SortField<*>>()
     private val fieldMapping = mutableMapOf<String, Field<*>>()
 
     override fun field(name: String): Field<*> {
         return fieldMapping[name] ?: throw IllegalStateException("Order field '$name' not support.")
     }
 
+    override fun default(): List<SortField<*>> {
+        return defaultOrder
+    }
+
     fun field(member: String, field: Field<*>) {
         fieldMapping[member] = field
         fieldMapping[member.toCamelCase()] = field
+    }
+
+    fun default(vararg fields: SortField<*>) {
+        defaultOrder += fields
     }
 }
 
--- middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/order/OrderByBuilder.kt
+++ middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/order/OrderByBuilder.kt
@@ -1,21 +1,28 @@
 package com.bybutter.sisyphus.middleware.jdbc.support.proto.order
 
+import com.bybutter.sisyphus.dsl.ordering.OrderDsl
 import com.bybutter.sisyphus.dsl.ordering.grammar.OrderParser
 import org.jooq.Field
 import org.jooq.SortField
 
 abstract class OrderByBuilder {
-    fun visit(orderBy: OrderParser.StartContext): List<SortField<*>> {
+    fun build(orderBy: String): List<SortField<*>> {
+        return visit(OrderDsl.parse(orderBy)).takeIf { it.isNotEmpty() } ?: default()
+    }
+
+    protected abstract fun default(): List<SortField<*>>
+
+    protected fun visit(orderBy: OrderParser.StartContext): List<SortField<*>> {
         return visit(orderBy.expr() ?: return listOf())
     }
 
-    fun visit(sortField: OrderParser.ExprContext): List<SortField<*>> {
+    protected fun visit(sortField: OrderParser.ExprContext): List<SortField<*>> {
         return sortField.order().map {
             visit(it)
         }
     }
 
-    fun visit(sortField: OrderParser.OrderContext): SortField<*> {
+    protected fun visit(sortField: OrderParser.OrderContext): SortField<*> {
         val field = field(sortField.field().text)
 
         if (sortField.DESC() != null) {
