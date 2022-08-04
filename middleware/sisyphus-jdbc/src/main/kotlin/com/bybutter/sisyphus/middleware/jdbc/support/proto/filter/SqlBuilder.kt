c00bd7da478eb894743e0e288e94161b33caacbc
Kanro <higan@live.cn>
2022-08-04 18:53:00 +0800

    :sparkles: Sql builder support select custom fields (#452)

--- middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/filter/SqlBuilder.kt
+++ middleware/sisyphus-jdbc/src/main/kotlin/com/bybutter/sisyphus/middleware/jdbc/support/proto/filter/SqlBuilder.kt
@@ -6,6 +6,7 @@ import com.bybutter.sisyphus.protobuf.primitives.Timestamp
 import com.bybutter.sisyphus.protobuf.primitives.toLocalDateTime
 import com.bybutter.sisyphus.protobuf.primitives.toTime
 import org.jooq.DSLContext
+import org.jooq.Field
 import org.jooq.Record
 import org.jooq.SelectConditionStep
 import java.util.concurrent.TimeUnit
@@ -14,10 +15,18 @@ abstract class SqlBuilder<T : Record> {
     abstract val runtime: FilterRuntime
 
     open fun select(dsl: DSLContext, filter: String): SelectConditionStep<T> {
-        return select(dsl, FilterVisitor.DEFAULT.build(this, filter))
+        return selectFields(dsl, filter)
     }
 
-    abstract fun select(dsl: DSLContext, expressions: List<Any?>): SelectConditionStep<T>
+    open fun selectFields(dsl: DSLContext, filter: String, vararg fields: Field<*>): SelectConditionStep<T> {
+        return buildSelect(dsl, FilterVisitor.DEFAULT.build(this, filter), *fields)
+    }
+
+    protected abstract fun buildSelect(
+        dsl: DSLContext,
+        expressions: List<Any?>,
+        vararg fields: Field<*>
+    ): SelectConditionStep<T>
 
     abstract fun member(member: FilterParser.MemberContext): Any?
 
