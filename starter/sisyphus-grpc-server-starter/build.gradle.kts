092d2e4787c2e535bcba0672f346409e649fac41
higan <higan@live.cn>
2022-08-24 10:41:34 +0800

    :sparkles: Health check api support

--- starter/sisyphus-grpc-server-starter/build.gradle.kts
+++ starter/sisyphus-grpc-server-starter/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
 protobuf {
     packageMapping(
         "io.grpc.reflection.v1" to "com.bybutter.sisyphus.starter.grpc.support.reflection.v1",
-        "io.grpc.reflection.v1alpha" to "com.bybutter.sisyphus.starter.grpc.support.reflection.v1alpha"
-    )
+        "io.grpc.reflection.v1alpha" to "com.bybutter.sisyphus.starter.grpc.support.reflection.v1alpha",
+        "io.grpc.health.v1" to "com.bybutter.sisyphus.starter.grpc.support.health.v1",
+        )
 }
