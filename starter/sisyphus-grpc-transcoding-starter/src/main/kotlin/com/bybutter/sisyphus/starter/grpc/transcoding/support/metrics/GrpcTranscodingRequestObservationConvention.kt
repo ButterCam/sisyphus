4ea48a6ad5345d9d4b65cfef788258d3bc27c5a7
higan <higan@live.cn>
2023-08-07 16:29:07 +0800

    :bug: Fix meter id name is null

--- starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/metrics/GrpcTranscodingRequestObservationConvention.kt
+++ starter/sisyphus-grpc-transcoding-starter/src/main/kotlin/com/bybutter/sisyphus/starter/grpc/transcoding/support/metrics/GrpcTranscodingRequestObservationConvention.kt
@@ -4,10 +4,11 @@ import com.bybutter.sisyphus.starter.grpc.transcoding.TranscodingFunctions
 import com.bybutter.sisyphus.starter.grpc.transcoding.TranscodingRouterRule
 import com.google.api.pathtemplate.PathTemplate
 import io.micrometer.common.KeyValues
+import org.springframework.http.server.reactive.observation.DefaultServerRequestObservationConvention
 import org.springframework.http.server.reactive.observation.ServerRequestObservationContext
-import org.springframework.http.server.reactive.observation.ServerRequestObservationConvention
 
-class GrpcTranscodingRequestObservationConvention : ServerRequestObservationConvention {
+class GrpcTranscodingRequestObservationConvention : DefaultServerRequestObservationConvention() {
+
     override fun getLowCardinalityKeyValues(context: ServerRequestObservationContext): KeyValues {
         val rule = context.attributes[TranscodingFunctions.TRANSCODING_RULE_ATTRIBUTE] as? TranscodingRouterRule
         val pathTemplate = context.attributes[TranscodingFunctions.MATCHING_PATH_TEMPLATE_ATTRIBUTE] as? PathTemplate
@@ -21,13 +22,13 @@ class GrpcTranscodingRequestObservationConvention : ServerRequestObservationConv
             )
         }
 
-        return KeyValues.of(
-            "grpc_method",
-            rule.method.methodDescriptor.fullMethodName,
-            "grpc_service",
-            rule.service.serviceDescriptor.name,
-            "uri",
-            pathTemplate.toString()
+        return super.getLowCardinalityKeyValues(context).and(
+            KeyValues.of(
+                "grpc_method",
+                rule.method.methodDescriptor.fullMethodName,
+                "grpc_service",
+                rule.service.serviceDescriptor.name
+            )
         )
     }
 }
